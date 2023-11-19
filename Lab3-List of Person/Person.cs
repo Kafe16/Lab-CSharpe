60f85407ff572bf4c1320120f3b3aeeca6e3b2a9
HAK ARUNAPHEA <87401628+Kafe16@users.noreply.github.com>
2023-11-19 13:49:07 +0700

    Add files via upload

---
+++ Lab3-List of Person/Person.cs
@@ -0,0 +1,57 @@
+﻿namespace _05_Person_With_Sort_Function
+{
+    public class Person {
+
+        // Instance Fields
+        protected string _name = "";
+        protected string _gender = "";
+        protected int _age = 0;
+    
+        public string getName() { return _name; }
+        public string getGender() { return _gender;}
+        public int getAge() { return _age;}
+
+        public static string getBar(char s = '-') 
+        {
+            return new string(s, 30 + 10 + 9 + 2);
+        }
+
+        public bool SetData(string data, string separator = "")
+        {
+            string[] arr = data.Split(separator);
+            if (arr.Length < 3) return false;
+            if (int.TryParse(arr[2], out _age)== false) return false; 
+            _name = arr[0].Trim();
+            _gender = arr[1].Trim();
+            
+            return true;
+        }
+
+        public string getInfo() => $"{_name,-30}{_gender, -10}{_age, 9}";
+        public static string getHeading() => $"{"Name", -30}{"Gender", -10}{"Age",9}";
+
+        // Static Methods:
+        /*
+        public static List<Person> GetPerson (string fileName)
+        {
+            Console.SetIn(File.OpenText(fileName));
+            var result = new List<Person>();
+            // add Data into List
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if(line == null) break;
+                //Person person = new Person();
+                Person person = new();
+
+                if (person.SetData(line, "/") == false) continue;
+                result.Add(person);
+            }
+                
+            return result;
+        }
+        */
+        // Method to output
+        
+    }
+}
---
+++ Lab3-List of Person/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _05_Person_With_Sort_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<--- Console App Reading Data into list --->");
+
+            Person per = new();
+            Console.WriteLine("\n<--- Read all data from File ---->");
+            List<Person> person = GetPerson("People.txt");
+            ViewPeople(person);
+            // Create List to store Ppl greater 25
+            Console.WriteLine("\n<--- This section is Output about Sort By Age > 25 --->");
+            List<Person> filterpeople = FilterForOverAge(person, 25);
+            filterpeople.Sort(ageComparison);
+            ViewPeople(filterpeople);
+
+            Console.WriteLine("\n<--- This section is Output about Sort By Name --->");
+            person.Sort(nameComparison);
+            ViewPeople(person);
+        }
+        
+        public static List<Person> GetPerson(string fileName)
+        {
+            Console.SetIn(File.OpenText(fileName));
+            var result = new List<Person>();
+            // add Data into List
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == null) break;
+                //Person person = new Person();
+                Person person = new();
+
+                if (person.SetData(line, "/") == false) continue;
+                result.Add(person);
+            }
+
+            return result;
+        }
+
+        public static void ViewPeople(List<Person> pers)
+        {
+            // output Heading 
+            Console.WriteLine(Person.getHeading());
+            Console.WriteLine(Person.getBar('='));
+
+            foreach (var p in pers)
+                Console.WriteLine(p.getInfo());
+
+            Console.WriteLine(Person.getBar('='));
+        }
+
+        public static List<Person> FilterForOverAge(List<Person> pers, int targetAge)
+        {
+            var result = new List<Person>();
+            foreach (var p in pers)
+                if (p.getAge() > targetAge) result.Add(p);
+
+            return result;
+        }
+        // Descending
+        static int ageComparison(Person x, Person y)
+        {
+            return y.getAge().CompareTo(x.getAge());
+        }
+
+        // Ascending
+        static int nameComparison(Person x, Person y)
+        {
+            return x.getName().CompareTo(y.getName());
+        }
+    }
+}
