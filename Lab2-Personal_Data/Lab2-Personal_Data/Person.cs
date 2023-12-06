7bf7d4387feb2add3b7d095018ff0bcaa24068bd
HAK ARUNAPHEA <87401628+Kafe16@users.noreply.github.com>
2023-12-06 14:44:30 +0700

    Add files via upload

---
+++ Lab2-Personal_Data/Lab2-Personal_Data/Person.cs
@@ -0,0 +1,43 @@
+﻿/*
+ 
+    Given a file people.txt with content:
+        - Heng Long/Male/23
+        = Keo Nary/Female/26
+        - San Metrey/Female/33
+        - Chan Rosa/Female/27
+        - Sok Vanny/Male/18
+
+    Write console Application to read people data from the given file, then out put  their in formation:
+
+    Name            Gender          Age
+    -----------------------------------
+    Heng Long       Male            23
+    
+
+    ----------------------------------
+
+    * Using a class to represent a person's data
+ 
+    + setData() , getInfo(),
+ */
+
+public class Person
+{
+    string outputFile = "person.txt";
+
+    protected string name = "";
+    protected string gender = "";
+    protected int age = 0;
+
+    public string getInfo() => $"{name,-30}{gender,-9}{age,3}"; // String Enterpolation. string តម្រឹមឆ្វេង so indent neg 
+    public bool setData(string data, string separate=" ")
+    {
+        string[] seg = data.Split(separate);
+        if (seg.Length < 3) return false;
+        if (int.TryParse(seg[2], out int ag)==false) return false;
+        name = seg[0].Trim(); // Trim() = ["----- AB -----"] => ["AB"]
+        gender = seg[1].Trim();
+        age = ag;
+        return true;
+    }
+}
---
+++ Lab2-Personal_Data/Lab2-Personal_Data/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ 
+    Given a file people.txt with content:
+        - Heng Long/Male/23
+        = Keo Nary/Female/26
+        - San Metrey/Female/33
+        - Chan Rosa/Female/27
+        - Sok Vanny/Male/18
+
+    Write console Application to read people data from the given file, then out put  their in formation:
+
+    Name            Gender          Age
+    -----------------------------------
+    Heng Long       Male            23
+    
+
+    ----------------------------------
+
+    * Using a class to represent a person's data
+    * 
+    * Method for class: setData() - getInfo() - 3 fields 
+ 
+ */
+
+Console.WriteLine("-------- OutPut Data from File --------");
+//var peopelData = new List<Person>();
+string file = "person.txt";
+Console.SetIn(File.OpenText(file));
+
+string result = "";
+
+while (true)
+{
+    string line = Console.ReadLine();
+    if(string.IsNullOrEmpty(line)) break;
+    Person p = new Person(); //== Person p = new();
+    if (p.setData(line, "/") == false) continue;
+    if (result != "") result += "\n";
+    result += p.getInfo();
+}
+
+string heading = $"{"Name",-30}{"Gender",-9}{"Age",3}";
+string bar = new string('-', 41);
+
+Console.WriteLine(heading);
+Console.WriteLine(bar);
+Console.WriteLine(result);
+Console.WriteLine(bar);
+
